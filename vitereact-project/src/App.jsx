33f2aeaf7bfd25addc7fa82e117f4b34672946c5
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-07-03 20:24:58 +0530

    adding some internal work and additional point

--- vitereact-project/src/App.jsx
+++ vitereact-project/src/App.jsx
@@ -1,12 +1,14 @@
 import Chai from "./chai" 
 
+//{} = called evaluated Expression means outcome of expression
 function App() {
-  
+  const username="take extra chai"
   return (
     <div>
-      <h1>chai or code with vite</h1>
+      <h1>chai or code with vite {username}</h1> /* {}= it use to treat as variable or final outcome of a variable */
       <Chai/>
       <h3>how is tea?</h3>
+      <Chai/>  
     </div>
   )
 }
--- vitereact-project/src/main.jsx
+++ vitereact-project/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
  
+/* 
+//Create element using tag
+  const anotherElement=(
+    <a href="https://google.com" target='_blank'>click me to visit google</a>
+  )
 
+  //variable pass after ancertag
+  const anotherUser= "another way"
+  //create Element tree using react 
+  const reactElement= react.createElement(
+    'a',
+    {href:'https://google.com',target:'_blank'},
+    'click me to visit google'
+    anotherUser // work as evaluated Expression like in App.jsx 
+  )
+*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    /* anotherElement */
+    /* reactElement */
     <App />
   </React.StrictMode>,
 )
