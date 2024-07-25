17325710fbc7c272e2af96abfa4cf12151084e5c
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-07-25 16:49:57 +0530

    some unique added

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, createRoutesFromElements, Route, Router, RouterProvider} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-
+import Github,{githubInfoLoader} from './components/Github/Github.jsx'
+/*
+//router creation Type one
 const router=createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
     children:[
       {
-        path: "",
+        path: "home",
         element: <Home />
       },
       {
@@ -28,6 +30,19 @@ const router=createBrowserRouter([
     ]
   }
 ])
+*/
+
+//router creation Type two
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='home' element={<Home />} />
+      <Route path='about' element={<About />}/>
+      <Route path='contact' element={<Contact />}/>
+      <Route loader={githubInfoLoader} path='github' element={<Github />}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
