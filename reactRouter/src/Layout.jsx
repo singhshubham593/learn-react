17e14eb37b05df4ea006c77e64b29731f67860d1
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-07-25 16:50:32 +0530

    Update Layout.jsx

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
+// Outlet 
+
 function Layout() {
     return (
         <> 
