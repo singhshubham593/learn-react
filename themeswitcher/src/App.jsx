9b50666d92171884a7d37d4eeaa3c269ecbdabac
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-12-24 13:13:42 +0530

    Created Todo list using ContextApi and store data in local

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -29,11 +29,9 @@ function App() {
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
           <ThemeBtn />
-           
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-           
           <Card />
         </div>
       </div>
---
+++ todoContextLocatStorage/src/componrnts/index.js
@@ -0,0 +1,4 @@
+import TodoForm from "./TOdoForm";
+import Todoitem from "./Todoitem";
+
+export {TodoForm,Todoitem}
